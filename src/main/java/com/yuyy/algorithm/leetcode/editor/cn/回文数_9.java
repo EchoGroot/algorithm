ab895baea14e034eec91a1e7481d4c4afbe518c3
余游洋 <yuyy114@126.com>
2022-03-12 17:11:30 +0800

    家里的deepin

---
+++ src/main/java/com/yuyy/algorithm/leetcode/editor/cn/回文数_9.java
@@ -0,0 +1,18 @@
+package com.yuyy.algorithm.leetcode.editor.cn;
+
+public class 回文数_9{
+
+	public static void main(String[] args) {
+		Solution solution = new 回文数_9().new Solution();
+		
+	}
+	
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/yuyy/algorithm/leetcode/editor/cn/特殊等价字符串组_893.java
@@ -0,0 +1,18 @@
+package com.yuyy.algorithm.leetcode.editor.cn;
+
+public class 特殊等价字符串组_893{
+
+	public static void main(String[] args) {
+		Solution solution = new 特殊等价字符串组_893().new Solution();
+		
+	}
+	
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numSpecialEquivGroups(String[] words) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
